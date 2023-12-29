0a49e59000b5ae828e8091dae7754578459af577
tieuquyngok1995 <tieuquyngok1995@gmail.com>
2023-12-29 14:56:37 +0700

    commit src

---
+++ ToolMovingFiles/ToolMovingFiles/Main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ToolMovingFiles
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
