89575aa93895388492c9dbeab004813f2cb85eaf
Vu Quoc Tuan <tieuquyngok1995@gmail.com>
2024-01-02 10:38:31 +0700

    commit src

--- ToolMovingFiles/ToolMovingFiles/Main.Designer.cs
+++ ToolMovingFiles/ToolMovingFiles/Main.Designer.cs
@@ -30,21 +30,364 @@ namespace ToolMovingFiles
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.txtPathFolder = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.progressBarFolder = new System.Windows.Forms.ProgressBar();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.btnCopyResult = new System.Windows.Forms.Button();
+            this.btnClearResult = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnOpenFolder);
+            this.groupBox1.Controls.Add(this.txtPathFolder);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.groupBox1.Location = new System.Drawing.Point(9, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(418, 77);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Path Moving Files";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Destination Folder";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::ToolMovingFiles.Properties.Resources.icon_folder_16x16;
+            this.button1.Location = new System.Drawing.Point(386, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 24);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.textBox1.Location = new System.Drawing.Point(131, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(250, 24);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Source Folder";
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Image = global::ToolMovingFiles.Properties.Resources.icon_folder_16x16;
+            this.btnOpenFolder.Location = new System.Drawing.Point(386, 15);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(26, 24);
+            this.btnOpenFolder.TabIndex = 4;
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            // 
+            // txtPathFolder
+            // 
+            this.txtPathFolder.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.txtPathFolder.Location = new System.Drawing.Point(131, 15);
+            this.txtPathFolder.Name = "txtPathFolder";
+            this.txtPathFolder.ReadOnly = true;
+            this.txtPathFolder.Size = new System.Drawing.Size(250, 24);
+            this.txtPathFolder.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.groupBox2.Location = new System.Drawing.Point(9, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(418, 77);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Path Moving Files";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Destination Folder";
+            // 
+            // button2
+            // 
+            this.button2.Image = global::ToolMovingFiles.Properties.Resources.icon_folder_16x16;
+            this.button2.Location = new System.Drawing.Point(386, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 24);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.textBox2.Location = new System.Drawing.Point(131, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(250, 24);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label4.Location = new System.Drawing.Point(6, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Source Folder";
+            // 
+            // button3
+            // 
+            this.button3.Image = global::ToolMovingFiles.Properties.Resources.icon_folder_16x16;
+            this.button3.Location = new System.Drawing.Point(386, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 24);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.textBox3.Location = new System.Drawing.Point(131, 15);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(250, 24);
+            this.textBox3.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.groupBox3.Location = new System.Drawing.Point(9, 149);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(418, 77);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Path Moving Files";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label5.Location = new System.Drawing.Point(6, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Destination Folder";
+            // 
+            // button4
+            // 
+            this.button4.Image = global::ToolMovingFiles.Properties.Resources.icon_folder_16x16;
+            this.button4.Location = new System.Drawing.Point(386, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(26, 24);
+            this.button4.TabIndex = 7;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.textBox4.Location = new System.Drawing.Point(131, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(250, 24);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label6.Location = new System.Drawing.Point(6, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Source Folder";
+            // 
+            // button5
+            // 
+            this.button5.Image = global::ToolMovingFiles.Properties.Resources.icon_folder_16x16;
+            this.button5.Location = new System.Drawing.Point(386, 15);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(26, 24);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.textBox5.Location = new System.Drawing.Point(131, 15);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(250, 24);
+            this.textBox5.TabIndex = 3;
+            // 
+            // progressBarFolder
+            // 
+            this.progressBarFolder.Location = new System.Drawing.Point(9, 231);
+            this.progressBarFolder.Name = "progressBarFolder";
+            this.progressBarFolder.Size = new System.Drawing.Size(418, 21);
+            this.progressBarFolder.TabIndex = 11;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnClearResult);
+            this.groupBox4.Controls.Add(this.btnCopyResult);
+            this.groupBox4.Controls.Add(this.txtResult);
+            this.groupBox4.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.groupBox4.Location = new System.Drawing.Point(9, 250);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(418, 75);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Logs";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtResult.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(3, 19);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(329, 53);
+            this.txtResult.TabIndex = 8;
+            this.txtResult.Text = "";
+            // 
+            // btnCopyResult
+            // 
+            this.btnCopyResult.Enabled = false;
+            this.btnCopyResult.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.btnCopyResult.Image = global::ToolMovingFiles.Properties.Resources.icon_copy_16x16;
+            this.btnCopyResult.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCopyResult.Location = new System.Drawing.Point(337, 14);
+            this.btnCopyResult.Name = "btnCopyResult";
+            this.btnCopyResult.Size = new System.Drawing.Size(75, 25);
+            this.btnCopyResult.TabIndex = 9;
+            this.btnCopyResult.Text = "    Copy";
+            this.btnCopyResult.UseVisualStyleBackColor = true;
+            // 
+            // btnClearResult
+            // 
+            this.btnClearResult.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.btnClearResult.Image = global::ToolMovingFiles.Properties.Resources.icon_clear_16x16;
+            this.btnClearResult.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClearResult.Location = new System.Drawing.Point(337, 43);
+            this.btnClearResult.Name = "btnClearResult";
+            this.btnClearResult.Size = new System.Drawing.Size(75, 25);
+            this.btnClearResult.TabIndex = 12;
+            this.btnClearResult.Text = "    Clear";
+            this.btnClearResult.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ClientSize = new System.Drawing.Size(436, 333);
+            this.Controls.Add(this.progressBarFolder);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox4);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = "Tool Moving Files";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.TextBox txtPathFolder;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ProgressBar progressBarFolder;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RichTextBox txtResult;
+        private System.Windows.Forms.Button btnClearResult;
+        private System.Windows.Forms.Button btnCopyResult;
     }
 }
 
